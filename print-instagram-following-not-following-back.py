3f73cfba981b740937dcf8108a2dc96bffa553ce
J. Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2022-03-03 12:20:27 -0500

    Create print-instagram-following-not-following-back.py

---
+++ print-instagram-following-not-following-back.py
@@ -0,0 +1,13 @@
+from instapy import InstaPy
+from instapy import smart_run
+
+session = InstaPy(username='',
+                  password='',
+                  headless_browser=False)
+
+with smart_run(session):
+    followers= session.grab_followers(username="", amount="full", live_match=True, store_locally=True)
+    following= session.grab_following(username="", amount="full", live_match=True, store_locally=True)
+    for account in following:
+        if account not in followers:
+            print(account)
